8068b3c89e8c36d0beb63e1f05b14e4618be9f03
nerdelt <nerdelt@gmail.com>
2018-08-07 13:51:31 -0500

    Assignment 5-1 First Attempt

---
+++ FullNames/FullNames/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FullNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //ask user for first name
+                Console.Write("Please enter your first name: ");
+                string firstName = Console.ReadLine();
+
+                //ask user for last name
+                Console.Write("Please enter your last name: ");
+                string lastName = Console.ReadLine();
+
+                //prints result, using FullName method
+                Console.Write("Hello, ");
+                FullName(firstName, lastName);
+                Console.ReadLine();     
+            }
+        }
+
+        // a string method which allows for two string inputs 
+        private  static string FullName(string firstName, string lastName)
+        {
+            // creates a new value that combines first and last name
+            string name = firstName + " " + lastName + "!";
+            Console.WriteLine(name);
+
+            //returns values 
+            return name;
+
+        }
+
+            /* Here is the alternative way I did it at first.
+             * I thought this was more condensed, but it didn't create a new value,
+             * and I couldn't make it a 'private static string', just 'private staic void'
+             * because I couldn't figure out how to return what was in the Console.WriteLine. 
+             * It worked, but I was nervous that I didn't correctly complete the 
+             * assignment and I didn't want to loose any points (like in the dreaded raffle).
+             * So if you could please advise why this way is wrong, or how to "return" 
+             * something using this:
+              
+
+                Console.WriteLine(String.Format("{0} {1}!", firstName, lastName));
+
+            */
+                
+               
+            }
+
+       
+        }
+    
+
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+
+
+            Console.Write("Hello, ");
+            FullName(firstName, lastName);
+
+            Console.ReadLine();
+
+         
+        }
+
+        private static void FullName(string firstName, string lastName)
+        {
+            //I don't know if the message I chose created a new value, 
+            //so I did this to show that I can create a new value for the method as well.
+            string name = firstName + " " + lastName;
+            Console.Writeline(name);
+
+           //Console.WriteLine(String.Format("{0} {1}!", firstName, lastName));
+           
+            
+        }
+
+       
+           }
+        }
+      
+    
+
