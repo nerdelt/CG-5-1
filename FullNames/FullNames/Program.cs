514b7996c4a35adf99feb7cbb4b49475a6a2d070
nerdelt <nerdelt@gmail.com>
2018-08-09 10:01:53 -0500

    Full Name updated method

--- FullNames/FullNames/Program.cs
+++ FullNames/FullNames/Program.cs
@@ -16,8 +16,9 @@ namespace FullNames
                 string lastName = Console.ReadLine();
 
                 //prints result, using FullName method
-                Console.Write("Hello, ");
-                FullName(firstName, lastName);
+                
+                string name = FullName(firstName, lastName);
+                Console.Write($"Hello,  {name}");
                 Console.ReadLine();     
             }
         }
@@ -27,27 +28,12 @@ namespace FullNames
         {
             // creates a new value that combines first and last name
             string name = firstName + " " + lastName + "!";
-            Console.WriteLine(name);
 
             //returns values 
             return name;
 
         }
 
-            /* Here is the alternative way I did it at first.
-             * I thought this was more condensed, but it didn't create a new value,
-             * and I couldn't make it a 'private static string', just 'private staic void'
-             * because I couldn't figure out how to return what was in the Console.WriteLine. 
-             * It worked, but I was nervous that I didn't correctly complete the 
-             * assignment and I didn't want to loose any points (like in the dreaded raffle).
-             * So if you could please advise why this way is wrong, or how to "return" 
-             * something using this:
-              
-
-                Console.WriteLine(String.Format("{0} {1}!", firstName, lastName));
-
-            */
-                
                
             }
 
